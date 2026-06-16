6c51cd0768a0ef82ecfbe3451fdab114272ade6c
noureddin <noureddin@protonmail.com>
2026-06-16 12:45:48 +0300

    convert all display:table to presentation tables

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ onresize = (ev) => {
     body.style.setProperty('--M', '100%')
     requestAnimationFrame(() => {
       body.style.setProperty('--R', width_of('.oskpref button') + 'px')
-      body.style.setProperty('--M', (width_of('.oskpref') - width_of('.oskpref label') - 2*REM) + 'px')
+      body.style.setProperty('--M', (width_of('.oskpref') - width_of('.oskpref td:first-child') - REM) + 'px')
     })
     //
     resize_canvas()
